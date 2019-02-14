c3a738bdb423503b4c010aa177a2504678749be4
Daniel Gut <daniel.gut@solarproject.ch>
2019-02-14 06:31:30 +0100

    fix(Unzip): await unzip (#21)
    * fix(Unzip): await unzip
    
    * fix(Zip): unzip reverted

--- nukeeper/src/NuKeeper.ts
+++ nukeeper/src/NuKeeper.ts
@@ -5,7 +5,7 @@ let https = require("follow-redirects").https;
 import fs = require("fs");
 import q = require("q");
 import { IncomingMessage } from "http";
-import * as extract from "extract-zip";
+import * as ttl from "vsts-task-tool-lib";
 
 async function execNuKeeper(args: string|string[]) : Promise<any>  {
     try {
@@ -37,24 +37,20 @@ async function run() {
    try {
         let localFilePath = path.join(__dirname, './', "nukeeper.nupkg");
         await downloadFile("https://www.nuget.org/api/v2/package/NuKeeper", localFilePath);
+        await ttl.extractZip(path.join(__dirname, './', 'nukeeper.nupkg'), path.resolve(__dirname, './nukeeper'));
         
-        let unzipOptions: extract.Options = {
-            dir: path.join(__dirname, "./nukeeper")
-        };
-        extract(localFilePath, unzipOptions, (err: Error) => tl.setResult(tl.TaskResult.Failed, err.message));
-      
         tl.exec("git", ["checkout", tl.getVariable('Build.SourceBranchName')]);
-        tl.exec("git", ["pull"]);
-        tl.exec("git", ["config", "--global", "user.name", "NuKeeper"]);
-        tl.exec("git", ["config", "--global", "user.email", "nukeeper@nukeeper.com"]);
-
-        tl.cd(tl.getVariable('Build.SourcesDirectory'));
-
-        let token = tl.getEndpointAuthorizationParameter("SystemVssConnection", "AccessToken", false);
-       
-        await execNuKeeper(['repo', tl.cwd(), token]);
-       
-        tl.setResult(tl.TaskResult.Succeeded, "done");
+            tl.exec("git", ["pull"]);
+            tl.exec("git", ["config", "--global", "user.name", "NuKeeper"]);
+            tl.exec("git", ["config", "--global", "user.email", "nukeeper@nukeeper.com"]);
+    
+            tl.cd(tl.getVariable('Build.SourcesDirectory'));
+    
+            let token = tl.getEndpointAuthorizationParameter("SystemVssConnection", "AccessToken", false);
+           
+            await execNuKeeper(['repo', tl.cwd(), token]);
+           
+            tl.setResult(tl.TaskResult.Succeeded, "done");
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err)
