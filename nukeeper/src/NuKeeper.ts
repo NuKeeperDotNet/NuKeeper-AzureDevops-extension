fbef66dcb945bac9554e28df8c0813c2563b631d
Marc Bruins <marc@marcbruins.nl>
2019-02-14 20:25:45 +0100

    remove old version of tool-lib

--- nukeeper/src/NuKeeper.ts
+++ nukeeper/src/NuKeeper.ts
@@ -1,8 +1,7 @@
 import * as path from 'path';
 import * as tl from 'vsts-task-lib';
 import * as tr from 'vsts-task-lib/toolrunner';
-import * as ttl from "vsts-task-tool-lib";
-import * as apl from "azure-pipelines-tool-lib";
+import * as ttl from "azure-pipelines-tool-lib";
 
 async function execNuKeeper(args: string|string[]) : Promise<any>  {
     try {
@@ -18,7 +17,7 @@ async function execNuKeeper(args: string|string[]) : Promise<any>  {
 
 async function run() {
    try {
-        let downPath: string = await apl.downloadTool("https://www.nuget.org/api/v2/package/NuKeeper", "nukeeper.nupkg");
+        let downPath: string = await ttl.downloadTool("https://www.nuget.org/api/v2/package/NuKeeper", "nukeeper.nupkg");
         await ttl.extractZip(downPath, path.resolve(__dirname, './nukeeper'));
         
         tl.exec("git", ["checkout", tl.getVariable('Build.SourceBranchName')]);
