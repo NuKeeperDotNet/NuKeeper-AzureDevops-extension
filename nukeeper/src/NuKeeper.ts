23bc834f32ef7724c1cf2d6816350b545d6567d4
Marc Bruins <marc@marcbruins.nl>
2019-02-14 20:20:27 +0100

    update

--- nukeeper/src/NuKeeper.ts
+++ nukeeper/src/NuKeeper.ts
@@ -1,11 +1,8 @@
 import * as path from 'path';
 import * as tl from 'vsts-task-lib';
 import * as tr from 'vsts-task-lib/toolrunner';
-let https = require("follow-redirects").https;
-import fs = require("fs");
-import q = require("q");
-import { IncomingMessage } from "http";
 import * as ttl from "vsts-task-tool-lib";
+import * as apl from "azure-pipelines-tool-lib";
 
 async function execNuKeeper(args: string|string[]) : Promise<any>  {
     try {
@@ -18,35 +15,16 @@ async function execNuKeeper(args: string|string[]) : Promise<any>  {
     }
 }
 
-async function downloadFile(url: string, dest: string): q.Promise<any> {
-    let deferal = q.defer<any>();
-    let file = fs.createWriteStream(dest);
-    https.get(url, (response: IncomingMessage) => {
-        response.pipe(file);
-        file.on("finish", () => {
-            deferal.resolve();
-        });
-    }).on("error", (err: any) => {
-        deferal.reject(err);
-    });
-
-    return deferal.promise;
-}
 
 async function run() {
    try {
-        let localFilePath = path.join(__dirname, './', "nukeeper.nupkg");
-        let extractionPath = path.resolve(__dirname, './nukeeper');
+        let downPath: string = await apl.downloadTool("https://www.nuget.org/api/v2/package/NuKeeper", "nukeeper.nupkg");
+        await ttl.extractZip(downPath, path.resolve(__dirname, './nukeeper'));
         
-        if (!fs.existsSync(extractionPath)) {
-            await downloadFile("https://www.nuget.org/api/v2/package/NuKeeper", localFilePath);
-            await ttl.extractZip(path.join(__dirname, './', 'nukeeper.nupkg'), path.resolve(__dirname, './nukeeper'));
-        }
-        
-        tl.execSync("git", ["checkout", tl.getVariable('Build.SourceBranchName')]);
-        tl.execSync("git", ["pull"]);
-        tl.execSync("git", ["config", "--global", "user.name", "NuKeeper"]);
-        tl.execSync("git", ["config", "--global", "user.email", "nukeeper@nukeeper.com"]);
+        tl.exec("git", ["checkout", tl.getVariable('Build.SourceBranchName')]);
+        tl.exec("git", ["pull"]);
+        tl.exec("git", ["config", "--global", "user.name", "NuKeeper"]);
+        tl.exec("git", ["config", "--global", "user.email", "nukeeper@nukeeper.com"]);
     
         tl.cd(tl.getVariable('Build.SourcesDirectory'));
     
