a6f37d8dcbf0a25417b38ee1aefffce46c16ca52
Marc Bruins <marc@marcbruins.nl>
2019-02-15 11:47:08 +0100

    update to 0.2.4

--- nukeeper/src/NuKeeper.ts
+++ nukeeper/src/NuKeeper.ts
@@ -2,11 +2,12 @@ import * as path from 'path';
 import * as tl from 'azure-pipelines-task-lib';
 import * as tr from 'azure-pipelines-task-lib/toolrunner';
 import * as ttl from 'azure-pipelines-tool-lib';
+import fs = require("fs");
 
 async function execNuKeeper(args: string|string[]) : Promise<any>  {
     try {
         return new tr.ToolRunner(tl.which("dotnet"))
-            .arg([path.join(__dirname, './nukeeper/tools/netcoreapp2.1/any', 'NuKeeper.dll')].concat(args))
+            .arg([path.join(tl.getVariable('Agent.TempDirectory'), './nukeeper/tools/netcoreapp2.1/any', 'NuKeeper.dll')].concat(args))
             .line(tl.getInput("arguments"))
             .exec();
     } catch (err){
@@ -16,10 +17,15 @@ async function execNuKeeper(args: string|string[]) : Promise<any>  {
 
 async function run() {
    try {
-        let downPath: string = await ttl.downloadTool("https://www.nuget.org/api/v2/package/NuKeeper", "nukeeper.nupkg");
-        await ttl.extractZip(downPath, path.resolve(__dirname, './nukeeper'));
-        
+        let extractionPath = path.resolve(tl.getVariable('Agent.TempDirectory'), './nukeeper');
+    
+        if (!fs.existsSync(extractionPath)) {
+            let downPath: string = await ttl.downloadTool("https://www.nuget.org/api/v2/package/NuKeeper", "nukeeper.nupkg");
+            await ttl.extractZip(downPath, extractionPath);
+        }
+
         tl.execSync("git", ["checkout", tl.getVariable('Build.SourceBranchName')]);
+        tl.execSync("git", ["pull"]);
         tl.execSync("git", ["config", "--global", "user.name", "NuKeeper"]);
         tl.execSync("git", ["config", "--global", "user.email", "nukeeper@nukeeper.com"]);
     
